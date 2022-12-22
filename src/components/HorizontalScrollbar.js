49688d895f639122dad636754d80affdaa30f1fb
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-12-22 11:47:20 +0100

    HorizontalScroll bar is in progress

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({data}) => {
   console.log(data);
@@ -11,7 +12,7 @@ const HorizontalScrollbar = ({data}) => {
         title={item.id || item }
         m="0 40px"
         >
-          {item}
+          <BodyPart item={item} />
         </Box>
     )
     )}
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,9 +3,8 @@ import React, {useEffect, useState} from 'react'
 import HorizontalScrollbar from './HorizontalScrollbar';
 import {  exerciseOptions, fetchData} from '../utils/fetchData';
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
         const [search, setSearch] = useState('');
-        const [exercises, setExercises] = useState([]);
         const [bodyParts, setBodyParts] = useState([]);
 
         useEffect(() => {
@@ -91,7 +90,11 @@ const SearchExercises = () => {
         </Box>
 
         <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
-            <HorizontalScrollbar data={bodyParts}  />
+            <HorizontalScrollbar 
+            data={bodyParts} 
+            setExercises={setExercises}
+            bodyPart ={bodyPart}
+            setBodyPart ={setBodyPart} />
         </Box>
 
     </Stack>
